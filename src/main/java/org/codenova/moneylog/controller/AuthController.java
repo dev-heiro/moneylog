798805f740f7e76eda806ebc01800b900ed060c1
heiro <dev.heiro@gmail.com>
2025-04-01 14:50:56 +0900

    feat : 로컬 및 카카오 로그인 구축

--- src/main/java/org/codenova/moneylog/controller/AuthController.java
+++ src/main/java/org/codenova/moneylog/controller/AuthController.java
@@ -4,15 +4,17 @@ package org.codenova.moneylog.controller;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.codenova.moneylog.entity.User;
+import org.codenova.moneylog.repository.UserRepository;
+import org.codenova.moneylog.request.LoginRequest;
 import org.codenova.moneylog.service.KakaoApiService;
 import org.codenova.moneylog.vo.KakaoTokenResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @AllArgsConstructor
@@ -20,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/auth")
 public class AuthController {
     private KakaoApiService kakaoApiService;
-
+    private UserRepository userRepository;
 
     @GetMapping("/login")
     public String loginHandle(Model model) {
@@ -33,18 +35,63 @@ public class AuthController {
     }
 
 
+    @PostMapping("/login")
+    public String loginPostHandle(
+            @ModelAttribute LoginRequest loginRequest,
+            HttpSession session,
+            Model model) {
+        User user =
+                userRepository.findByEmail(loginRequest.getEmail());
+        if(user != null && user.getPassword().equals(loginRequest.getPassword())) {
+            session.setAttribute("user", user);
+            return "redirect:/index";
+        }else {
+            return "redirect:/auth/login";
+        }
+    }
+
+
+    @GetMapping("/signup")
+    public String signupGetHandle(Model model) {
+
+        return "auth/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signupPostHandle(@ModelAttribute User user) {
+        User found = userRepository.findByEmail(user.getEmail());
+        if(found == null) {
+            user.setProvider("LOCAL");
+            user.setVerified("T");
+            userRepository.save(user);
+        }
+        return "redirect:/index";
+    }
+
     @GetMapping("/kakao/callback")
-    public String kakaoCallbackHandle(@RequestParam("code") String code) throws JsonProcessingException {
+    public String kakaoCallbackHandle(@RequestParam("code") String code,
+                                      HttpSession session
+                                      ) throws JsonProcessingException {
         // log.info("code = {}", code);
         KakaoTokenResponse response =kakaoApiService.exchangeToken(code);
         log.info("response.idToken = {}", response.getIdToken());
 
         DecodedJWT decodedJWT = JWT.decode(response.getIdToken());
-        String sub = decodedJWT.getClaim("sub").toString();
-        String nickname = decodedJWT.getClaim("nickname").toString();
-        String picture = decodedJWT.getClaim("picture").toString();
+        String sub = decodedJWT.getClaim("sub").asString();
+        String nickname = decodedJWT.getClaim("nickname").asString();
+        String picture = decodedJWT.getClaim("picture").asString();
 
-        log.info("decodedJWT: sub={}, nickname={}, picture={} ", sub, nickname, picture);
-        return "redirect:/";
+        User found = userRepository.findByProviderAndProviderId("KAKAO", sub);
+        log.info("found = {}", found);
+        if(found != null) {
+            session.setAttribute("user", found);
+        } else {
+            User user = User.builder().provider("KAKAO")
+                    .providerId(sub).nickname(nickname).picture(picture).verified("T").build();
+            userRepository.save(user);
+            session.setAttribute("user", user);
+        }
+
+        return "redirect:/index";
     }
 }
